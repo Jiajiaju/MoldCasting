d7f35a8f08e6ee658e2ebbf0b80919f4e914e57b
jiajia <jiajia@jiajia.com>
2026-07-26 18:41:43 +0800

    feat: add project game instance

---
+++ Source/MoldCasting/Public/MCGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MCGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOLDCASTING_API UMCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
